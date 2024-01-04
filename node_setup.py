2de885bab8b49f7e079988e42ba5a451db9e1cb9
PC-47 <32375595+PC-47@users.noreply.github.com>
2024-01-04 10:14:45 -0700

    Update node_setup.py
    fixed new lines

--- node_setup.py
+++ node_setup.py
@@ -14,31 +14,28 @@ def set_repos() -> None:
     print_task('setting subscription repo to pve-no-subscription')
     try:
         with open('/etc/apt/sources.list', 'w') as f:
-            f.write('deb http://ftp.us.debian.org/debian bookworm main contrib')
-            f.write('')
-            f.write('deb http://ftp.us.debian.org/debian bookworm-updates main contrib')
-            f.write('')
-            f.write('# security updates')
-            f.write('deb http://security.debian.org bookworm-security main contrib')
-            f.write('')
-            f.write('# pve no subscription repository')
-            f.write('deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription')
+            f.write('deb http://ftp.us.debian.org/debian bookworm main contrib\n\n')
+            f.write('deb http://ftp.us.debian.org/debian bookworm-updates main contrib\n\n')
+            f.write('# security updates\n')
+            f.write('deb http://security.debian.org bookworm-security main contrib\n\n')
+            f.write('# pve no subscription repository\n')
+            f.write('deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription\n')
     except Exception as e:
         print(f'ERROR: {e} while changing main sources list')
     
     print_task('disabling pve-enterprise repo')
     try:
         with open('/etc/apt/sources.list.d/pve-enterprise.list', 'w') as f:
-            f.write('# comment out')
-            f.write('# deb https://enterprise.proxmox.com/debian/pve bookworm pve-enterprise')
+            f.write('# comment out\n')
+            f.write('# deb https://enterprise.proxmox.com/debian/pve bookworm pve-enterprise\n')
     except Exception as e:
         print(f'ERROR: {e} while changing main sources list')
 
     print_task('disabling ceph enterprise repo')
     try:
         with open('/etc/apt/sources.list.d/ceph.list', 'w') as f:
-            f.write('# comment out')
-            f.write('# deb https://enterprise.proxmox.com/debian/ceph-quincy bookworm enterprise')
+            f.write('# comment out\n')
+            f.write('# deb https://enterprise.proxmox.com/debian/ceph-quincy bookworm enterprise\n')
     except Exception as e:
         print(f'ERROR: {e} while changing main sources list')
 
